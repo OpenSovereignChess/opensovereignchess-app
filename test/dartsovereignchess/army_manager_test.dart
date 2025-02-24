27d82d21b0c6dd42bdfbef31c2e417c40522cb7d
Al Nguyen <al.nguyen@gmail.com>
2025-02-24 07:01:57 -0500

    test: Add army manager test file for Darts Sovereign Chess

---
+++ test/dartsovereignchess/army_manager_test.dart
@@ -0,0 +1,11 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:opensovereignchess_app/dartsovereignchess/dartsovereignchess.dart';
+import 'package:opensovereignchess_app/dartsovereignchess/src/army_manager.dart';
+//import 'package:opensovereignchess_app/dartsovereignchess/src/debug.dart';
+
+void main() {
+  group('ArmyManager', () {
+    // Write tests for ArmyManager here ai!
+  });
+}
